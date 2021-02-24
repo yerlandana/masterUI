f54e7094fb00d3029a01cdfb6f6eef795b287b89
yerlandan <dyerlanova@gmail.com>
2021-02-24 14:47:56 +0600

    add data in firestore

--- app/src/main/java/com/example/m/MainActivity.java
+++ app/src/main/java/com/example/m/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.m;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.m.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+    ProgressDialog pd;
+    private FirebaseFirestore db;
+    private static final String TAG = "MainActvity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    private void initViews() {
+
+        db = FirebaseFirestore.getInstance();
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.saveBtn.setOnClickListener(v -> {
+            String title = mBinding.titleEt.getText().toString().trim();
+            String description = mBinding.descriptionEt.getText().toString().trim();
+            pd = new ProgressDialog(this);
+
+
+            uploadData(title, description);
+        });
+    }
+
+    private void uploadData(String title, String description) {
+        pd.setTitle("Adding ...");
+        pd.show();
+
+        String id = UUID.randomUUID().toString();
+        Map<String, Object> doc = new HashMap<>();
+        doc.put("id", id);
+        doc.put("title", title);
+        doc.put("description", description);
+
+        db.collection("Documents").document(id).set(doc)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Added", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    pd.setTitle("Failed");
+                    pd.show();
+                }
+            });
+
     }
 }
